932790af62df559908a2004b7a6d019f40456bd5
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2018-10-24 17:47:50 -0300

    adicionando exemplo

--- config.js
+++ config.js
@@ -1,34 +1,37 @@
 module.exports = {
 
     projectDir : "C:\\Users\\TOTVS\\workspace\\TBjGestaoInadimplenciaService\\tfs-gestaoinadimplencia-model-cadastro\\src\\main\\java",
+
     packageName: "com.totvs.tfs.gestaoinadimplencia.cadastro.model",
     packageNameDTO: "com.totvs.tfs.gestaoinadimplencia.cadastro.dto",
-    enconding: "latin1",
+    enconding: "latin1", //Condificação do arquivo, o padrão do eclipse é latin1 e não utf-8
 
     classes: [
 
         {
-            className: "Teste2",
-            nomeSequenceTabela: "s_gid_teste2",
-            superClassDTO: undefined, //Valor padrão = AbstractDTO | Opções: AbstractDTO ou DefaultDTO
+            className: "Acordo",
+            nomeSequenceTabela: "s_gid_acordo",
+            superClassDTO: undefined, //Se não informado o valor padrão é AbstractDTO | Opções: AbstractDTO ou DefaultDTO
             atributes: [
-                ["nome", "String", {
-                    size: { max: 10},
-                    digit: { integer: 4, fraction: 2}
-                }],
-                ["categoria", { type: "entity", getKey: "getCategoriaId"}],
-                ["tipoStatus", { type: "enum"}]
+                ["codAcordo","Long"],
+                ["codCpfCnpj","String"],
+                ["codCorporacao","String"],
+                ["dtAcordo","LocalDate"],
+                ["valPrincOri","Long"],
+                ["valMultaOri","Long"],
+                ["valMoraOri","Long"],
+                ["valEncargosOri","Long"],
+                ["valAtrasoOri","Long"],
+                ["valEntrada","Long"],
+                ["qtdProdutos","Long"],
+                ["perDescontoPrin","Long"],
+                ["perDescontoAtr ","Long"],
+                ["dtVctEntrada ","LocalDate"],
+                ["dtPrimVct ","LocalDate"],
+                ["codPlanoPrazo ","Long"],
+                ["idSitAcordo ","String"]
+
             ]
         },
-        {
-            className: "Teste",
-            nomeSequenceTabela: "s_gid_teste",
-            superClassDTO: undefined,
-            atributes: [
-                ["nome", "Long"],
-                ["descricao", "string"],
-                ["valor", "BigDecimal"],
-            ]
-        }
     ]
 }
